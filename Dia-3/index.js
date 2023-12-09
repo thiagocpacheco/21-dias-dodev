d1e2c7e23a9463fedd36ef5e7aa7b3b05c072666
Thiago Pacheco <thiago2003.cp@gmail.com>
2023-12-09 12:22:56 -0300

    Como resolvi o desafio do Dia 3 da DoDev

--- Dia-3/index.js
+++ Dia-3/index.js
@@ -1,1 +1,31 @@
-console.log("Esse é o dia 3");
+let seuNome = prompt("Qual o seu nome ");
+let suaIdade = Number(prompt("qual a sua idade"));
+let suaAltura = Number(
+  prompt(
+    "Qual a sua altura em m, por favor use ponto para declarar a altura e não vírgula"
+  )
+);
+let seuPeso = Number(prompt("Qual o seu peso"));
+
+let anoNascimento = 2023 - suaIdade;
+//alert("Você nasceu em " + anoNascimento);
+let alturaAoQuadrado = suaAltura * suaAltura;
+
+let imc = seuPeso / alturaAoQuadrado;
+//alert("Seu IMC é de " + imc + " Kg/m2");
+alert(
+  "Olá " +
+    seuNome +
+    "," +
+    " você tem " +
+    suaIdade +
+    " anos, nasceu em " +
+    anoNascimento +
+    ", tem " +
+    suaAltura +
+    " de altura, pesa " +
+    seuPeso +
+    "kg e seu IMC é de " +
+    imc +
+    " Kg/m2"
+);
